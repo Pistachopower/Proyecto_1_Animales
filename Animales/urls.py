4990e0dc71a328dd9b3c4353334d040347d04db6
Pistachopower <nelson.galicia.carrero.al@iespoligonosur.org>
2024-10-04 13:50:03 +0200

    El proyecto está arreglado

--- Animales/urls.py
+++ Animales/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.animal_list, name='animal_list'),
+    path('animal', views.animal_list, name='animal_list'),
     path('protectora/', views.protectora_list, name='protectora_list'),
     path('colaborador/', views.colaborador_list, name='colaborador_list'),
     
--- Animales/views.py
+++ Animales/views.py
@@ -3,19 +3,19 @@ from .models import Animal, Protectora, Colaborador
 
 # Create your views here.
 from django.shortcuts import render
-
+from .models import Animal, Protectora, Colaborador
 
 
 
 # Create your views here.
 def animal_list(request):
    animales = Animal.objects.all()
-   return render(request, 'animales/post_list.html', {"animal_mostrar":animales})
+   return render(request, 'animales/animal_list.html', {"animal_mostrar":animales})
 
 def protectora_list(request):
    protectora = Protectora.objects.all()
-   return render(request, 'animales/post_list.html', {"protectora_mostrar":protectora})
+   return render(request, 'animales/protectora_list.html', {"protectora_mostrar":protectora})
 
 def colaborador_list(request):
    colaborador = Colaborador.objects.all()
-   return render(request, 'animales/post_list.html', {"colaborador_mostrar":colaborador})
+   return render(request, 'animales/colaborador_list.html', {"colaborador_mostrar":colaborador})
